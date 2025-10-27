e8b4f5774ebffcf12cb7c61864eb993aa73262cc
Jason <jieshenlim352@gmail.com>
2025-10-27 19:19:01 +0800

    This is a branch for Register class

---
+++ src/Register.java
@@ -0,0 +1,5 @@
+public class Register{
+    public void registerUser(String username,String password,String email,String password2){
+        System.out.println("You have successfully registered an account under the name of: " + username + ", please check your email" + email + "!");
+    }
+}
